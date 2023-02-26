542a670eba9a51d1f3a22268dd6c26ac3b7c178a
DN8899 <derricknovak889@gmail.com>
2023-02-26 14:34:49 -0500

    Added retrieval of the date.

--- Publix Ad Scrapper/src/DataRetrievals.java
+++ Publix Ad Scrapper/src/DataRetrievals.java
@@ -1,4 +1,6 @@
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.Date;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -10,16 +12,18 @@ public class DataRetrievals extends ConnectDatabase {
 	ConnectDatabase connection = new ConnectDatabase();
 	
 	public DataRetrievals() {
-		
 	}
 	
 	
+	/* Get XAG for each premier league team 
+	 * put it into the database
+     */
 	public void retrieveXAG() throws ClassNotFoundException, SQLException {
 		String statsUrl = "https://fbref.com/en/comps/9/Premier-League-Stats";
 		String dataUrl = myVariables.getUrl();
 		String userName = myVariables.getUserName();
 		String password = myVariables.getPassword();
-		int count = 1;
+		
 		ConnectDatabase databaseConnection = new ConnectDatabase(dataUrl, userName, password);
 		
 		try {
@@ -29,7 +33,7 @@ public class DataRetrievals extends ConnectDatabase {
 			{
 				String xGName = row.select("[data-stat='xg_assist_per90']").text();	
 				xAFinal = xGName.split(" ");
-				System.out.println(xGName + "HI");
+				System.out.println(xGName);
 			}
 			for (int i = 1; i < 21; i++) {
 				System.out.println(xAFinal[i]);
@@ -37,8 +41,6 @@ public class DataRetrievals extends ConnectDatabase {
 						" SET xAG = " + xAFinal[i] +
 						" WHERE teamID = " + i;
 				databaseConnection.returnStmt(sql);
-				count++;
-			
 			}
 			
 		} catch(Exception e) {
@@ -49,13 +51,15 @@ public class DataRetrievals extends ConnectDatabase {
 		}
 	}
 	
-	
+	/* Get XG for each premier league team 
+	 * put it into the database
+     */
 	public void retrieveXG() throws ClassNotFoundException, SQLException {
 		String statsUrl = "https://fbref.com/en/comps/9/Premier-League-Stats";
 		String dataUrl = myVariables.getUrl();
 		String userName = myVariables.getUserName();
 		String password = myVariables.getPassword();
-		int count = 1;
+		
 		ConnectDatabase databaseConnection = new ConnectDatabase(dataUrl, userName, password);
 		
 		try {
@@ -73,8 +77,6 @@ public class DataRetrievals extends ConnectDatabase {
 						" SET xG = " + xGFinal[i] +
 						" WHERE teamID = " + i;
 				databaseConnection.returnStmt(sql);
-				count++;
-			
 			}
 			
 		} catch(Exception e) {
@@ -85,4 +87,32 @@ public class DataRetrievals extends ConnectDatabase {
 		}
 	}
 	
+	public LocalDate getDate() {
+		LocalDate date = LocalDate.now();
+		return date;
+	}
+	
+	public LocalDate getSevenDaysDate() {
+		LocalDate date = LocalDate.now();
+		LocalDate addedDate = date.plusDays(7);
+		return addedDate;
+	}
+	
+	public String getGameName() throws ClassNotFoundException, SQLException {
+		String dataUrl = myVariables.getUrl();
+		String userName = myVariables.getUserName();
+		String password = myVariables.getPassword();
+		
+		String sql = "select teamHome, teamAway from englandmatchmain \r\n"
+				+ "		where gametime between '2023-02-26' and '2023-03-05'\r\n"
+				+ "		AND teamHome = 'Arsenal' ";
+		ConnectDatabase databaseConnection = new ConnectDatabase(dataUrl, userName, password);
+		
+		databaseConnection.returnStmt(sql);
+		
+		//temp
+		return null;
+	}
+	
+
 }
--- Publix Ad Scrapper/src/Main.java
+++ Publix Ad Scrapper/src/Main.java
@@ -14,73 +14,15 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Main {
-	private static String dataUrl = "jdbc:mysql://localhost:3306/soccer_leagues";
-	private static String userName = "root";
-	private static String password = "DandB#8899";
-	
-	
 	
 	public static void main(String[] args) throws Exception {
 		DataRetrievals retrieve = new DataRetrievals();
-		/*
-		//SQL Variables
-		String dataUrl = "jdbc:mysql://localhost:3306/soccer_leagues";
-		String userName = "root";
-		String password = "DandB#8899";
 		
-		//Class.forName("com.mysql.jdbc.Driver");
-		Connection con = DriverManager.getConnection(dataUrl, userName, password);
-		Statement stmt = con.createStatement();
-
-		*/
+		retrieve.retrieveXG();
+		retrieve.retrieveXAG();
+		System.out.println(retrieve.getDate());
+		System.out.println(retrieve.getSevenDaysDate());
 		
-		
-		
-		
-		//-------------------------------------FIRST TEST AREA-------------------------------------
-		/*
-		try 
-		{
-			final Document doc = Jsoup.connect(url).get();
-			
-			
-			for (Element row : doc.select("table.shsTable.shsBorderTable tr"))
-			{
-				if (row.select("td.shsNamD:nth-of-type(1)").text().equals("")) 
-				{
-					continue;
-				}
-				else 
-				{
-					String result = row.select("td.shsNamD:nth-of-type(1)").text();
-					System.out.print("\n" + result);
-					//String times = row.select(".shsETZone.shsTimezone").text();
-					//System.out.println(times);
-					String nameHome = row.select("td.shsNamD:nth-of-type(2)").text();
-					System.out.print(" " + nameHome);
-					String nameAway = row.select("td.shsNamD:nth-of-type(3)").text();
-					System.out.print("-" + nameAway);
-				}
-			}
-			*/
-			//-------------------------------------FIRST TEST AREA ABOVE-------------------------------------
-			
-		
-		//final String bet = "https://fbref.com/en/comps/9/schedule/Premier-League-Scores-and-Fixtures";
-		String statsUrl = "https://fbref.com/en/comps/9/Premier-League-Stats";
-		
-		//Create database connection 
-		ConnectDatabase databaseConnection = new ConnectDatabase(dataUrl, userName, password);
-		
-		try {
-				
-			final Document statDoc = Jsoup.connect(statsUrl).get();
-			
-			//---------------------------------------Premier League Group-------------------------------------
-			
-			int count = 1; 	
-			
-			
 			//---------------------------------------XG AGAINST RETRIVIAL---------------------------------------
 		
 			/*
@@ -95,105 +37,7 @@ public class Main {
 			}
 			*/
 			
-			
-			
-			//--------------------------------------XA RETRIVIAL----------------------------------
-			
-			
-			retrieve.retrieveXAG();
-			
-			
-			//--------------------------------------------XG RETRIVIAL-------------------------------------------------------------
-			
-			
-			String[] xGFinal = new String[20];
-			for (Element row : statDoc.select("#stats_squads_standard_for"))			
-			{
-				String xGName = row.select("[data-stat='xg_per90']").text();	
-				xGFinal = xGName.split(" ");
-			}
-			/*
-			for (int i = 1; i < 21; i++) {
-				System.out.println(xGFinal[i]);
-				String sql = "UPDATE englandteams" + 
-						" SET xG = " + xGFinal[i] +
-						" WHERE teamID = " + i;
-				stmt.executeUpdate(sql);
-				count++;
-			
-			}
-			*/		
-			
 					
-					//----------------------HOME CODE-----------------------------
-					/*
-					StringBuilder home = new StringBuilder();
-					
-					String homeTemp = row.select("td.right:nth-of-type(4)").text();
-					String homeReplace = homeTemp.replaceAll("'", "");
-					home.append('\'');
-					home.append(homeReplace);
-					home.append('\'');
-					System.out.println(homeReplace);
-					
-					
-					
-					String sql = "INSERT INTO englandGames" + 
-								"(gameID ,teamHome) " +
-								"VALUES (" + count + ","+ home +");";
-					
-					System.out.println(sql);
-					stmt.executeUpdate(sql);
-					count++;
-					*/
-					
-					
-					
-					
-					//-----------------------------AWAY CODE-------------------------------------
-					/*StringBuilder away = new StringBuilder();
-					
-					String awayTemp = row.select("td.right:nth-of-type(8)").text();
-					String awayReplace = awayTemp.replaceAll("'", "");
-					away.append('\'');
-					away.append(awayReplace);
-					away.append('\'');
-					System.out.println(awayReplace);*/
-					
-					
-					
-					
-					
-					
-					/*------------------------------RESULT CODE---------------------------------------
-					String result = row.select("td.left:nth-of-type(2)").text();
-					System.out.println(result);
-					StringBuilder resultReplace = new StringBuilder();
-					resultReplace.append('\'');
-					resultReplace.append(result);
-					resultReplace.append('\'');
-					//Use update to input away and results
-					String sql = "UPDATE englandgames" + 
-								" SET gameTime = str_to_date("  + resultReplace + ", '%Y-%m-%d')" +
-								" WHERE gameID = " + count;
-					
-					System.out.println(sql);
-					stmt.executeUpdate(sql);
-					count++;
-					
-					//-------------------------------RESULT CODE------------------------------------------
-				}*/
-			
-		}
-		
-		catch (Exception ex) {
-			ex.printStackTrace();
-		}
-		finally {
-			databaseConnection.closeStatement();
-			databaseConnection.closeConnection();
-		} 
 		
 	}
-
 }
